50b13d165e0cdec48be901de79b6a0d98d74791a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-12 06:59:38 -0700

    fetch package name after install

--- src/anywherewill/__init__.py
+++ src/anywherewill/__init__.py
@@ -1,9 +1,24 @@
+import importlib.metadata
 import sys
 
 from . import main2
 
 
+def get_project_name():
+    try:
+        return importlib.metadata.metadata("anywherewill")["Name"]
+    except importlib.metadata.PackageNotFoundError:
+        return None
+
+
 def main() -> int:
+    project_name = get_project_name()
+    if project_name:
+        print("Project name:", project_name)
+    else:
+        print("Project name not found.")
+
     out = main2.render_template("extended.j2")
     sys.stdout.write(out)
+
     return 0
